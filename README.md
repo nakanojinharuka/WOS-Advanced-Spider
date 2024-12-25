88f2687caff49da6203b90d25dc7e9c7a42ef274
nakanojinharuka <ui_wither@163.com>
2024-12-25 21:49:49 +0800

    file submit

---
+++ README.md
@@ -0,0 +1,113 @@
+# WOS-spider
+Python script to crawl Web of Science retrieved papers based on selenium package.\
+基于selenium包，爬取Web of Science检索的论文信息的Python脚本
+
+## 思路
+
+
+The idea of this crawler is inherited and taken off from [CNKI Knowledge Web Crawler](https://github.com/Dramwig/CNKI-spider). The new version of Web of Science does not require an account to search as CNKI does, and Web of Science generally requires the purchase of an account or the use of library databases to jump. Because the many and varied ways of accessing can only be handled manually, and thus thinking: can we interrupt the program to manually control the browser, and later give control to the program to do so? And then came the code.
+
+In summary, this crawler has the following enhancements:
+
+- **Retrieval method:** ability to manually do the retrieval in any way by itself, as long as it ends up on the paper page
+- **Speed:** using BeautifulSoup to process the html of the web page as crawling data, compared to Selenium to crawl each data much faster
+- **Stability:** Web of Science is unstable to access in China because of the change of [way], you can solve the problem manually after the webpage has problems, or you can start crawling from a custom page
+
+这个爬虫的思路继承和脱胎自[CNKI知网爬虫](https://github.com/Dramwig/CNKI-spider)。新版Web of Science并不像知网一样不需要账号也能检索，而Web of Science一般需要购买账号或者使用图书馆数据库转跳。因为多种多样的的进入方式只能通过手动处理，进而思考：我们是否可以中断程序手动控制浏览器，之后再把控制权交给程序进行？然后就有了这样的代码。
+
+综上所述，这个爬虫有了以下提升：
+
+- **检索方式**：能自己手动进行任意方式检索，只要最后能进入论文页面
+- **速度**：使用BeautifulSoup对网页html处理为爬取数据，相比Selenium对每个数据抓取快得多
+- **稳定性**：Web of Science在国内访问不稳定，因为[方式]的改变，可以在网页出现问题后手动解决，也可以从自定义页面开始爬取
+
+## 实现
+
+The whole program implements automatic traversal of Web of Science paper detail pages, extracts key information, and saves the results in an orderly manner in a CSV file.
+
+1. Define the `parse_html` function, which receives an HTML string as an argument. The function parses the HTML content with BeautifulSoup, extracts relevant information about the paper (e.g., title, number of citations, address/country, journal name, etc.), and stores this information in a dictionary. It also tries to get the page number from the HTML where the current paper is located.
+
+2. in the `__main__` part of the main function:
+   - Initializes the Chrome driver and opens the specified URL (Web of Science's base search page).
+   - Ask the user to manually navigate the browser to the paper details page.
+   - Switch to the browser window corresponding to the paper details page.
+   - Enter a loop that processes the paper details page one paper at a time:
+     - Wait for the page elements to finish loading using Selenium's WebDriverWait.
+     - Get the HTML source code of the page and pass it to the `parse_html` function to process it and get a dictionary of the paper's information and the current page number.
+     - Add the paper information to a pandas DataFrame and update the data if the index already exists, otherwise add a new row.
+     - After each paper is processed, save its data to a CSV file.
+     - Try to click the "Next" button to scroll to the next article's detail page, if it fails prompt the user to manually fix the possible problem.
+     - Add a pause time to wait for the page to load. 3.
+
+3. close the browser driver when the loop is complete.
+
+整个程序实现了自动遍历Web of Science论文详情页面，抽取关键信息，并将结果有序地保存在CSV文件中。
+
+1. 定义`parse_html`函数，接收一个HTML字符串作为参数。该函数通过BeautifulSoup解析HTML内容，提取出论文的相关信息（如标题、引用次数、地址/国家、期刊名等），并将这些信息存入一个字典中。同时，它还尝试从HTML中获取当前论文所在的页码。
+
+2. 在主函数`__main__`部分：
+   - 初始化Chrome浏览器驱动并打开指定的URL（Web of Science的基础搜索页面）。
+   - 要求用户手动操作浏览器至论文详情页面。
+   - 切换到论文详情页面对应的浏览器窗口。
+   - 进入循环，每次循环处理一篇论文的详情页面：
+     - 使用Selenium的WebDriverWait等待页面元素加载完成。
+     - 获取页面的HTML源代码并传给`parse_html`函数处理，得到论文信息字典以及当前页码。
+     - 将论文信息添加到一个pandas DataFrame中，如果索引已存在则更新数据，否则新增一行。
+     - 每处理完一篇论文后，就将其数据保存到CSV文件中。
+     - 尝试点击“下一页”按钮以滚动到下一篇文章的详情页面，若点击失败则提示用户手动解决可能的问题。
+     - 添加暂停时间以等待页面加载。
+
+3. 循环结束后，关闭浏览器驱动。
+
+## 使用
+
+### 环境
+
+运行方式：脚本运行
+
+本地环境：Python 3.11.5，selenium 4.15.2，pandas 2.0.3，beautifulsoup4 4.12.2
+
+### 可变参数
+
+```python
+url_root = 'https://webofscience-clarivate-cn-s.era.lib.swjtu.edu.cn/wos/alldb/basic-search'
+papers_need = 100000
+file_path = 'result.csv'    
+wait_time = 10
+pause_time = 3
+```
+- EN
+   -  `url_root`: is the page that is opened automatically (can be any page, as it has to be done manually)
+   - `papers_need`: automatically stop crawling the number of pages
+   - `file_path`: the path to store the crawl result table file
+   - `wait_time`: time (in seconds) to wait for an element of the page to load, can be shortened
+   - `pause_time`: wait time after each page turn (in seconds), balance speed and stability, can be shortened
+- CN
+   - `url_root`：是自动打开的网页（可以是任意网页，因为要手动操作）
+   - `papers_need`：自动停止爬取的页数
+   - `file_path`：爬取结果表格文件存储路径
+   - `wait_time`：等待网页某元素加载的时间（秒），可以缩短
+   - `pause_time`：每次翻页后的等待时间（秒），平衡速度和稳定性，可以缩短
+
+### 使用步骤
+
+Optional: whether or not to open excel. used to choose whether or not to continue to run next to the last crawled data, if it is the first time to use input n can be.
+
+1. Run the program. It will automatically open Google Chrome, (make sure Google **chromedriver** is installed)
+2. Manually.
+   1. enter Web of Science (the new version of the interface), use your own way to enter (no can Taobao buy account)
+   2. Select the database you want by your own way, enter the keywords, and click Search.
+   3. Click the first article to enter the article page
+3. Crawl automatically. Input any key to continue in the program terminal, the program will automatically crawl the page information and store it in the csv table.
+4. *Exception Handling: If the web page is accessed abnormally, click "Back" to go back to the previous interface, then input any key to continue, and continue crawling.
+
+
+可选：是否打开excel。用于选择是否接着上次爬取的数据继续跑，如果是第一次使用输入n即可。
+
+1. 运行程序。会自动打开谷歌浏览器，（确保安装了谷歌**chromedriver**，可以参考[Selenium安装WebDriver最新Chrome驱动](https://blog.csdn.net/Z_Lisa/article/details/133307151)）
+2. 手动操作。
+   1. 进入Web of Science（新版界面），使用自己的方式进入（没有可以淘宝买账号）
+   2. 通过自己想要的方式，选择想要的数据库，输入关键词，点击检索
+   3. 点击第一篇文章，进入文章页面
+3. 自动爬取。在程序终端输入任意键继续，程序会自动爬取页面信息，并存储在csv表格中。
+4. *异常处理：网页访问异常，点击“后退”回到上个界面，再输入任意键继续，继续爬取。
---
+++ WOS_spider.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+"""
+File: WOS_spider.py
+Author: Dramwig
+Email: dramwig@outlook.com
+Date: 2024-02-27
+Version: 1.6
+
+Description: This script uses Selenium and BeautifulSoup to scrape detailed paper information from Web of Science (WOS) website.
+It navigates through each paper's detail page, extracts key information such as title, citation count, country, journal, etc., 
+and saves the collected data into a CSV file.
+
+Please note that this script is intended for educational purposes only, and you should abide by the terms of service and usage policies 
+of the Web of Science when using it or any derivative work.
+
+"""
+import selenium.webdriver.chrome.service as service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+import time, keyboard
+import winsound
+from webdriver_manager.chrome import ChromeDriverManager
+
+# 解析html
+def parse_html(html):
+    index = 0
+    soup = BeautifulSoup(html, 'html.parser')
+    # 创建一个空的字典
+    data_dict = {}
+    try:
+        containers = soup.find_all('div', class_='cdx-two-column-grid-container')
+        for container in containers:
+            # 在这个容器内找到所有的标签和数据
+            labels = container.find_all(class_='cdx-grid-label')
+            datas = container.find_all(class_='cdx-grid-data')
+            class_authors = datas[0].text.strip()
+            label = labels[0].text.strip()
+            data_texts = [data.text.strip() for data in datas] # 提取数据列表中的文本
+            text = '\n'.join(data_texts) # 将文本连接成一个字符串，使用换行符分隔
+            # 存储到字典中
+            data_dict['author'] = class_authors
+            break
+    except Exception as error:
+        print("解析容器/获取作者失败")
+    try:
+        class_title = soup.select('html > body > app-wos > main > div > div > div:nth-child(2) > div > div > div:nth-child(2) > app-input-route > app-full-record-home > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > app-full-record > div > div:nth-child(1) > div:nth-child(1) > div > div > h2')
+        data_dict['title'] = class_title[0].text.strip()
+        print('\t'+class_title[0].text.strip())
+    except Exception as error:
+        print("获取标题失败")
+    time.sleep(0.3)
+    try:
+        journal=''
+        conference=''
+        #                                        'mat-tooltip-trigger mat-menu-trigger category section-label-data identifiers-link ng-star-inserted'
+        j_find = soup.find_all('a', class_='mat-menu-trigger font-size-14 summary-source-title-link source-title-link remove-space no-left-padding section-label-data identifiers-link ng-star-inserted')
+        #                                        'mat-menu-trigger font-size-14 summary-source-title-link source-title-link remove-space no-left-padding section-label-data identifiers-link ng-star-inserted'
+        c_find = soup.find_all('span', class_='summary-source-title noLink ng-star-inserted')
+        if len(j_find) != 0:
+            journal = j_find[0].contents[0].text.strip()
+        elif len(c_find) != 0:
+            conference = c_find[0].text.strip()
+        print('\t'+journal if bool(journal) else '\t'+conference)
+        data_dict['journal/conference/book'] = journal if bool(journal) else conference
+    except Exception as error:
+        print("获取所在期刊、书目或会议名失败")
+    time.sleep(0.3)
+    try:
+        publishyear = ''
+        earlyaccess = ''
+        class_year_publishyear = soup.find_all('span', class_='value section-label-data', id='FullRTa-pubdate')
+        class_year_earlyaccess = soup.find_all('span', class_='value section-label-data', id='FullRTa-earlyAccess')
+        if len(class_year_publishyear) != 0:
+            publishyear = class_year_publishyear[0].text.strip()
+        elif len(class_year_earlyaccess) != 0:
+            earlyaccess = class_year_earlyaccess[0].text.strip()
+        data_dict['publish time'] = publishyear if bool(publishyear) else earlyaccess
+    except Exception as error:
+        print("获取时间失败")
+    time.sleep(0.3)
+    try:
+        class_citation = soup.find_all('div', class_='citation-position citation-count ng-star-inserted')
+        data_dict['citation'] = class_citation[0].contents[0].text.strip()
+    except Exception as error:
+        try:
+            class_citation = soup.find_all('div', class_='citation-two-column')
+            data_dict['citation'] = class_citation[0].contents[0].contents[0].text.strip()
+        except Exception as error:
+            data_dict['citation'] = '0'
+    time.sleep(0.3)
+    try:
+        abstract = soup.find_all('div', class_='abstract--instance abstract-size section-label-data', id='FullRTa-abstract-basic')
+        data_dict['abstract'] = abstract[0].text.strip()
+    except Exception as error:
+        print("摘要未给出")
+    time.sleep(0.3)
+    try:
+        keywords = ''
+        class_keywords = soup.find_all('app-full-record-keywords', class_='ng-star-inserted')
+        if len(class_keywords[0].text.strip()) == 0:
+            try:
+                class_keywords_controlled = soup.find_all('app-full-record-categories-classification', class_='ng-star-inserted')
+                if len(class_keywords_controlled) != 0:
+                    try:
+                        keywords = class_keywords_controlled[0].contents[0].contents[17].contents[0].contents[1].text.strip()
+                        data_dict['keywords'] = keywords
+                    except Exception as error:
+                        print("关键词未给出")
+                        data_dict['keywords'] = ''
+            except Exception as error:
+                print("关键词未给出")
+                data_dict['keywords'] = ''
+        else:
+            keywords = class_keywords[0].contents[0].contents[1].text.strip()
+            data_dict['keywords'] = keywords
+    except Exception as error:
+        print("获取关键词失败")
+        data_dict['keywords'] = ''
+    time.sleep(0.3)
+    try:
+        input_box = soup.find(class_='wos-input-underline page-box')  # 获取包含输入框的标签
+        index = int(input_box['aria-label'].split()[-1].replace(",", ""))
+    except Exception as error:
+        print("获取数量失败")
+    time.sleep(0.3)
+    return index, data_dict
+
+
+if __name__ == "__main__":
+    url_root = 'https://www.webofscience.com/wos/alldb/basic-search'
+    papers_need = 1000
+    file_path = 'urban_renewal.csv'
+    wait_time = 3
+    pause_time = 1
+    # 变量
+    judge_xpath = '//*[@id="FullRRPTa-useInWOS"]'
+    xpath_nextpaper = '/html/body/app-wos/main/div/div/div[2]/div/div/div[2]/app-input-route/app-full-record-home/div[1]/app-page-controls/div/form/div/button[2]'
+    df = pd.DataFrame()
+    index = 0
+    duration = 2000  # 提示音时间 millisecond
+    freq = 440  # 提示音Hz
+    flag = 0
+    # 读取df
+    if_read = input("是否读取已有的CSV文件？(y/n)")
+    if if_read == 'y':
+        df = pd.read_csv(file_path, index_col=0)
+        index = int(df.index[-1].split('_')[-1])
+        print(f"读取已有的CSV文件，当前行索引为{index},即第{index+1}篇论文")
+    # 创建ChromeOptions对象
+    chrome_options = webdriver.ChromeOptions()
+    # 禁止加载图片等资源
+    chrome_options.add_argument("--disable-images")
+    chrome_options.add_argument("--disable-plugins")
+    chrome_options.add_argument("--disable-extensions")
+    # 创建浏览器对象
+    chrome_service = service.Service(ChromeDriverManager().install())
+    # 创建WebDriver对象时传入ChromeOptions
+    driver = webdriver.Chrome(options=chrome_options, service=chrome_service)
+    driver.get(url_root) # 打开的页面
+    # 手动操作，比如切换标签页等
+    input("请手动操作至论文详情页面,完成后按Enter键继续...")
+    # 获取获取当前所有窗口的句柄
+    window_handles = driver.window_handles
+    # 假设新窗口是最后被打开的
+    new_window_handle = window_handles[-1]
+    # 切换到新窗口
+    driver.switch_to.window(new_window_handle)
+    # 在新窗口上进行操作，例如获取新窗口的标题
+    print("新窗口的标题(请确保页面正确):", driver.title)
+    while index <= papers_need:
+        print("正在处理第", index+1, "篇论文")
+        # 等待页面加载
+        try:
+            # 或者等待直到某个元素可见
+            element = WebDriverWait(driver, wait_time).until(EC.visibility_of_element_located((By.XPATH, judge_xpath)))
+        except Exception as e:
+            print("等待超时，页面不存在该元素，也可能是页面加载失败")
+        time.sleep(pause_time)
+        # 解析HTML
+        try:
+            html = driver.page_source
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath_nextpaper))).click() # 切换到下一页
+            index, data = parse_html(html)
+            row_index = f'Row_{index}'
+            if row_index in df.index:
+                df.loc[row_index] = pd.Series(data, name=row_index) # 如果行索引存在，则覆盖对应行的数据
+            else:
+                df = df._append(pd.Series(data, name=row_index)) # 如果行索引不存在，则追加新的行
+            df.to_csv(file_path, index=True)  # 将DataFrame保存为CSV文件,保留行索引作为第一列
+            if keyboard.is_pressed('down') or keyboard.is_pressed('0'):  # 按下键盘上的向下键或者数字0 进入中断检查
+                t = input("程序中断，输入Enter键继续，其他用于调试...")
+                while t != '':
+                    html = driver.page_source
+                    index,data = parse_html(html)
+                    t = input("程序中断，输入Enter键继续...")
+            flag = flag - 1
+        except Exception as e:
+            print("An error occurred:", e)
+            if flag <= 0:
+                print("尝试重新加载页面...")
+                flag = 2
+                driver.back()
+                time.sleep(pause_time)
+            else:
+                winsound.Beep(freq, duration)  # 提示音
+                input("网页出现问题等待手动解决...")
+    # 关闭浏览器
+    driver.quit()
