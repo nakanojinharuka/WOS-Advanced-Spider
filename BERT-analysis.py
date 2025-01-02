b6399a80c77f31dec1d031b83ffd84b2a7e68c19
nakanojinharuka <ui_wither@163.com>
2025-01-02 17:02:10 +0800

    daily altering function

--- BERT-analysis.py
+++ BERT-analysis.py
@@ -15,7 +15,7 @@ stopwords1 = ['the', 'and', 'of', 'in', 'to', 'for', 'a', 'on', 'is', 'with', 'a
              'value', 'approach', 'use', 'utilize', 'increase', 'decrease', 'build', 'space', 'finding', 'old', 'cellular',
              'young', 'plan', 'significant', 'important', 'framework', 'explore', 'relationship', 'key', 'property', 'wr',
              'keywords', 'keyword', 'author', 'plus', 'multi', 'learn', 'information', 'population', 'algorithm', 'auto',
-             'diffusion', 'game', 'social', 'community', 'china', 'policy', 'politic', 'right', 'spatial', 'race', 'impacts'
+             'diffusion', 'game', 'social', 'community', 'china', 'policy', 'politic', 'right', 'spatial', 'race', 'impacts',
              'politics', 'participate', 'participation', 'rights', 'state', 'gen', 'post', 'projects', 'land', 'cover', 'two',
              'neural', 'patterns', 'pattern', 'eva', 'ag', 'learning', 'risk', 'that', 'by', 'from', 'was', 'as', 'results',
              'are', 'sic', 'areas', 'based', 'we', 'can', 'changes', 'using', 'be', 'between', 'models', 'which', 'where',
@@ -25,9 +25,14 @@ stopwords1 = ['the', 'and', 'of', 'in', 'to', 'for', 'a', 'on', 'is', 'with', 'a
              'their', 'its', 'ours', 'remote sensing', 'yang river', 'previous studies', 'con network', 'degrees degrees',
              'driving factors', 'not only', 'time series', 'patch generating', 'through', 'such', 'been', 'have been', 'built',
              'difference', 'differences', 'or', 'existing', 'structure', 'buildings', 'potential', 'factors', 'studies',
-             'time', 'street', 'show', 'support', 'findings', 'strategies', 'government', 'within']
+             'time', 'street', 'show', 'support', 'findings', 'strategies', 'government', 'within', 'mobility', 'travel',
+             'human', 'co', 'understanding', 'spat', 'rural', 'activity', 'activities', 'network', 'networks', 'transport',
+             'transportation', 'location', 'traffic', 'daily', 'among', 'large', 'com', 'geo', 'socio', 'access', 'scale',
+             'cluster', 'spaces', 'mobile', 'big', 'di', 'trip', 'trips', 'gender', 'gender', 'relate', 'related', 'sc',
+             'due', 'movement', 'one', 'well']
 stopwords2 = ['view images', 'structural racism', 'random forest', 'carried out', 'hong kong', 'new york', 'coordination degree',
-              'overall accuracy', 'recent years', 'difference differences']
+              'overall accuracy', 'recent years', 'difference differences', 'human mobility', 'united states', 'mode choice',
+              'real world', 'jobs housing']
 stopwords3 = nltk.download('stopwords')
 nltk_stopwords3 = set(stopwords.words('english'))
 # 2. 加载 BERT(-T5L) 分词器
@@ -61,9 +66,9 @@ def t5_tokenize(text):
 # 4. 定义main函数
 def main_proceed():
     # 5. 加载数据
-    df = pd.read_csv("sorted/sorted_urban_renewal_OFFICIAL.csv")  # 替换为实际文件路径
+    df = pd.read_csv("sorted/sorted_urban_mobility_patterns_OFFICIAL.csv")  # 替换为实际文件路径
     # 6. 指定需要处理的文本列（支持多列）
-    columns_to_process = [f'keyword{i}' for i in range(18)] + ['abstract']  # 替换为你的列名列表
+    columns_to_process = [f'keyword{i}' for i in range(21)] + ['abstract']  # 替换为你的列名列表
     # 7. 处理多列文本数据
     all_texts = []  # 存储所有列的分词结果
     for col in columns_to_process:
@@ -85,21 +90,21 @@ def main_proceed():
 if __name__=='__main__':
     # 11. 运行main函数
     word_freq_list = main_proceed()
-    # 12. 输出前20个最常见的词
-    print("前20个最常见的词频：", dict(list(word_freq_list.items())[:20]))
+    # 12. 输出前25个最常见的词
+    print("前25个最常见的词频：", dict(list(word_freq_list.items())[:25]))
     # 13. 绘制词频图
     rcParams['font.sans-serif'] = ['SimHei', 'Times New Roman']  # 可以根据需要换成其他字体
     rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
     plt.figure(figsize=(16, 12))
-    # 提取前20个高频二元短语及其频率
-    phrases = list(word_freq_list.keys())[:20]
-    frequencies = list(word_freq_list.values())[:20]
+    # 提取前25个高频二元短语及其频率
+    phrases = list(word_freq_list.keys())[:25]
+    frequencies = list(word_freq_list.values())[:25]
     # 绘制横向条形图
     plt.barh(phrases, frequencies)  # barh表示横向条形图
     plt.xlabel("频率")
-    plt.ylabel("短语")
-    plt.title("短语频率统计")
+    plt.ylabel("短语/单词")
+    plt.title("频率统计")
     plt.gca().invert_yaxis()  # 反转 y 轴，使频率最高的短语在顶部
-    pd.DataFrame(list(word_freq_list.items()),columns=['Phrase', 'Frequency']).to_csv("tables/urban_renewal_frequency_OFFICIAL.csv",
-                                                                                      index=False)
+    # pd.DataFrame(list(word_freq_list.items()),columns=['Phrase', 'Frequency']).to_csv("tables/urban_mobility_patterns_frequency_OFFICIAL.csv",
+    #                                                                                   index=False)
     plt.show()
